96bec379615589b3f72ae35ed07ef2fdddbaeaba
ameyagoel <ameyagoel@gmail.com>
2023-02-09 13:55:15 -0800

    finished landing

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -8,7 +8,7 @@ const navigation = () => {
   return (
     <div style={{paddingLeft: '1rem', backgroundColor:' rgba(96, 107, 153, .4)', position:'fixed', width: '100vw', zIndex: 1}}>
       <a className='navbtnImg'></a>
-      <a href='#contact' className='navbtntText'>| About |</a>
+      <a href='#about' className='navbtntText'>| About |</a>
       <a href='#contact' className='navbtntText'>Portfolio |</a>
       <a href='#contact' className='navbtntText'>Work | </a>
     </div>
