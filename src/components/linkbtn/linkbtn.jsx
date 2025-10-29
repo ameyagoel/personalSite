9fcc792d34043c83c4594d7a8d179eaad6e90b0a
Your Name <you@example.com>
2025-10-28 18:37:31 -0700

    fixes + contact module

--- src/components/linkbtn/linkbtn.jsx
+++ src/components/linkbtn/linkbtn.jsx
@@ -9,9 +9,9 @@ const Link = () => {
       <Row>
         <a className='linkd' href='https://www.linkedin.com/in/ameya-goel-14b4181b5/' target="_blank"></a>
       </Row>
-      {/* <Row>
+      <Row>
         <a className='email' href='mailto:ameyagoel@gmail.com?subject=Contact Inquiry' target="_blank"></a>
-      </Row> */}
+      </Row>
 
       <Row>
         <a className='git' href="https://github.com/ameyagoel" target="_blank"></a>
